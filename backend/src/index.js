7f79fa9ea0f9f5707dbc16299122c1d2615a246f
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-09-28 23:47:37 -0300

    Finalizado BackEnd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 const app = express();
 require('./config/dbConfig');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+const port = 3333;
+app.listen(port, () => {
+    console.log(`O servidor está executando na porta: ${port}`);
+});
