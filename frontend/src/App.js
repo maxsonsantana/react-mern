809e86f5265c976902738a6c9c5ba849c6681ca3
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-10-04 23:57:14 -0300

    Start FrontEnd

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+
+function App() {
+  return (  
+    <div>
+      <Header title="Minha Nota 1"/>
+      <Header title="Minha Nota 2"/>
+      <Header title="Minha Nota 3"/>      
+    </div>  
+  );
+}
+
+export default App;
