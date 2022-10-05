385039782abc7d9df0f9d3ed3df1c0959a172414
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-10-05 00:07:41 -0300

    Componentes e Estados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Header from './Header';
 
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function handleSoma(){
+    setCounter(counter + 1);
+  }
+
+  function handleMenos(){
+    setCounter(counter - 1);
+  }
+
   return (  
-    <div>
+    <>
+      <h1>{counter}</h1>
+      <button onClick={handleSoma}>+</button>
+      <button onClick={handleMenos}>-</button>
+
       <Header title="Minha Nota 1"/>
       <Header title="Minha Nota 2"/>
       <Header title="Minha Nota 3"/>      
-    </div>  
+    </>  
   );
 }
 
